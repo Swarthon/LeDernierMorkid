d1db82d0e43b7b4b8eed5e2f2ffa9b5375477f18
Swarthon <swarthon.gokan@gmail.com>
2017-06-05 12:09:37 +0200

    Creating LeDernierMorkid class

---
+++ include/LeDernierMorkid.h
@@ -0,0 +1,49 @@
+#ifndef _LEDERNIERMORKID_H
+#define _LEDERNIERMORKID_H_
+
+#include "GraphicsSystem.h"
+#include "LogicSystem.h"
+#include "GameEntityManager.h"
+#include "GraphicsGameState.h"
+#include "LogicGameState.h"
+
+#include "Threading/YieldTimer.h"
+
+#include <OgreRenderWindow.h>
+#include <OgreTimer.h>
+
+#include <Threading/OgreThreads.h>
+#include <Threading/OgreBarrier.h>
+
+#include <iostream>
+
+using namespace Common;
+
+extern const double cFrametime;
+
+class LeDernierMorkid {
+public:
+	LeDernierMorkid();
+	void run();
+protected:
+	struct ThreadData {
+		GraphicsSystem  *graphicsSystem;
+		LogicSystem     *logicSystem;
+		Ogre::Barrier   *barrier;
+	};
+
+	static unsigned long renderThread(Ogre::ThreadHandle *threadHandle);
+	static unsigned long renderThreadApp(Ogre::ThreadHandle *threadHandle);
+	static unsigned long logicThread(Ogre::ThreadHandle *threadHandle);
+
+	static THREAD_DECLARE( renderThread );
+	static THREAD_DECLARE( logicThread );
+
+	GraphicsGameState	*mGraphicsGameState;
+	LogicGameState		*mLogicGameState;
+	GraphicsSystem		*mGraphicsSystem;
+	LogicSystem		*mLogicSystem;
+	GameEntityManager	*mGameEntityManager;
+};
+
+#endif // _LEDERNIERMORKID_H_
---
+++ src/LeDernierMorkid.cpp
@@ -0,0 +1,153 @@
+#include "LeDernierMorkid.h"
+
+const double cFrametime = 1.0 / 25.0;
+
+//---------------------------------------------------------------------
+LeDernierMorkid::LeDernierMorkid() {
+	mGraphicsGameState = new GraphicsGameState();
+	mLogicGameState = new LogicGameState();
+
+ 	mGraphicsSystem = new GraphicsSystem(mGraphicsGameState);
+	mLogicSystem = new LogicSystem(mLogicGameState);
+
+	mGraphicsGameState->_notifyGraphicsSystem(mGraphicsSystem);
+	mLogicGameState->_notifyLogicSystem(mLogicSystem);
+
+	mGraphicsSystem->_notifyLogicSystem(mLogicSystem);
+	mLogicSystem->_notifyGraphicsSystem(mGraphicsSystem);
+
+	mGameEntityManager = new GameEntityManager(mGraphicsSystem, mLogicSystem);
+}
+//---------------------------------------------------------------------
+void LeDernierMorkid::run() {
+	Ogre::Barrier barrier( 2 );
+	ThreadData threadData;
+	threadData.graphicsSystem   = mGraphicsSystem;
+	threadData.logicSystem      = mLogicSystem;
+	threadData.barrier          = &barrier;
+
+	Ogre::ThreadHandlePtr threadHandles[2];
+	threadHandles[0] = Ogre::Threads::CreateThread( THREAD_GET( renderThread ), 0, &threadData );
+	threadHandles[1] = Ogre::Threads::CreateThread( THREAD_GET( logicThread ), 1, &threadData );
+
+	Ogre::Threads::WaitForThreads( 2, threadHandles );
+}
+
+//---------------------------------------------------------------------
+unsigned long LeDernierMorkid::renderThread(Ogre::ThreadHandle *threadHandle) {
+	unsigned long retVal = -1;
+
+	try {
+		retVal = renderThreadApp( threadHandle );
+	} catch( Ogre::Exception& e ) {
+	#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+		MessageBoxA( NULL, e.getFullDescription().c_str(), "An exception has occured!",
+					 MB_OK | MB_ICONERROR | MB_TASKMODAL );
+	#else
+		std::cerr << "An exception has occured: " <<
+					 e.getFullDescription().c_str() << std::endl;
+	#endif
+		abort();
+	}
+	return retVal;
+}
+//---------------------------------------------------------------------
+unsigned long LeDernierMorkid::renderThreadApp(Ogre::ThreadHandle *threadHandle) {
+	ThreadData	*threadData	= reinterpret_cast<ThreadData*>(threadHandle->getUserParam());
+	GraphicsSystem	*graphicsSystem	= threadData->graphicsSystem;
+	Ogre::Barrier	*barrier	= threadData->barrier;
+
+	graphicsSystem->initialize("Le Dernier Morkid");
+	barrier->sync();
+
+	if(graphicsSystem->getQuit()){
+		graphicsSystem->deinitialize();
+		return 0; //User cancelled config
+	}
+
+	graphicsSystem->createScene();
+	barrier->sync();
+
+	Ogre::RenderWindow *renderWindow = graphicsSystem->getRenderWindow();
+
+	Ogre::Timer timer;
+	unsigned long startTime = timer.getMicroseconds();
+	double timeSinceLast = 1.0 / 60.0;
+
+	while(!graphicsSystem->getQuit()){
+		graphicsSystem->beginFrameParallel();
+		graphicsSystem->update( timeSinceLast );
+		graphicsSystem->finishFrameParallel();
+
+		if(!renderWindow->isVisible()){
+			//Don't burn CPU cycles unnecessary when we're minimized.
+			Ogre::Threads::Sleep( 500 );
+		}
+
+		unsigned long endTime = timer.getMicroseconds();
+		timeSinceLast = (endTime - startTime) / 1000000.0;
+		timeSinceLast = std::min( 1.0, timeSinceLast ); //Prevent from going haywire.
+		startTime = endTime;
+	}
+
+	barrier->sync();
+
+	graphicsSystem->destroyScene();
+	barrier->sync();
+
+	graphicsSystem->deinitialize();
+	barrier->sync();
+
+	return 0;
+}
+//---------------------------------------------------------------------
+unsigned long LeDernierMorkid::logicThread(Ogre::ThreadHandle *threadHandle){
+	ThreadData	*threadData	= reinterpret_cast<ThreadData*>(threadHandle->getUserParam());
+	GraphicsSystem	*graphicsSystem	= threadData->graphicsSystem;
+	LogicSystem	*logicSystem	= threadData->logicSystem;
+	Ogre::Barrier	*barrier	= threadData->barrier;
+
+	logicSystem->initialize();
+	barrier->sync();
+
+	if(graphicsSystem->getQuit()){
+		logicSystem->deinitialize();
+		return 0; //Render thread cancelled early
+	}
+
+	logicSystem->createScene();
+	barrier->sync();
+
+	Ogre::RenderWindow *renderWindow = graphicsSystem->getRenderWindow();
+
+	Ogre::Timer timer;
+	YieldTimer yieldTimer( &timer );
+
+	unsigned long startTime = timer.getMicroseconds();
+
+	while(!graphicsSystem->getQuit()){
+		logicSystem->beginFrameParallel();
+		logicSystem->update( static_cast<float>( cFrametime ) );
+		logicSystem->finishFrameParallel();
+
+		logicSystem->finishFrame();
+
+		if(!renderWindow->isVisible()){
+			//Don't burn CPU cycles unnecessary when we're minimized.
+			Ogre::Threads::Sleep( 500 );
+		}
+
+		//YieldTimer will wait until the current time is greater than startTime + cFrametime
+		startTime = yieldTimer.yield( cFrametime, startTime );
+	}
+
+	barrier->sync();
+
+	logicSystem->destroyScene();
+	barrier->sync();
+
+	logicSystem->deinitialize();
+	barrier->sync();
+
+	return 0;
+}
